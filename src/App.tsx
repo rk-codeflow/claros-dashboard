312c7ed17ff1a02ddd0656a2afcfe043fc5b8c4d
Raj Kiran Chaudhary <42111832+rk-codeflow@users.noreply.github.com>
2025-11-20 15:34:04 +0545

    custom scrollbar added

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         isOpen={sidebarOpen}
       />
 
-      <main className="flex flex-col h-screen overflow-y-auto space-y-3 transition-all duration-300 w-full bg-whitesmoke">
+      <main className="flex flex-col h-screen overflow-y-auto space-y-3 transition-all duration-300 w-full bg-whitesmoke custom-scrollbar">
         <Header />
         {currentPage === "home" && <Home />}
         {currentPage === "data" && <Data />}
